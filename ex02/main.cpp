f36b536cfc856ac9a29f4968fec7d963f521e43c
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-04-22 12:00:06 +0200

    add colors to output for more readability

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,19 +1,22 @@
 #include "Fixed.h"
 #include <iostream>
 
+#define BOLDMAGENTA "\033[1m\033[35m"
+#define RESET       "\033[0m"
+
 int main( void ) {
     Fixed       a;
     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
+    std::cout << BOLDMAGENTA << a << RESET << std::endl;
+    std::cout << BOLDMAGENTA << ++a << RESET << std::endl;
+    std::cout << BOLDMAGENTA << a << RESET << std::endl;
+    std::cout << BOLDMAGENTA << a++ << RESET << std::endl;
+    std::cout << BOLDMAGENTA << a << RESET << std::endl;
 
-    std::cout << b << std::endl;
+    std::cout << BOLDMAGENTA << b << RESET << std::endl;
 
-    std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << BOLDMAGENTA << Fixed::max( a, b ) << RESET << std::endl;
 
     return 0;
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,12 +1,15 @@
 #include "Point.h"
 #include <iostream>
 
+#define BOLDMAGENTA "\033[1m\033[35m"
+#define RESET       "\033[0m"
+
 int main( void ) {
     Point a {1.2, 3.4};
-    std::cout << "a is " << a << '\n';
+    std::cout << BOLDMAGENTA << "a is " << a << RESET << '\n';
 
     Point b {a};
-    std::cout << "b is " << b << '\n';
+    std::cout << BOLDMAGENTA << "b is " << b << RESET << '\n';
 
     return 0;
 }
